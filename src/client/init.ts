eb9a4af56e8857afdc5fbf64fba0d250795788d2
ju4tlearn <ju4tlearn@gmail.com>
2024-09-27 18:41:59 +0800

    :art: 整合初始化代码

---
+++ src/client/init.ts
@@ -0,0 +1,23 @@
+import { createApp, type App as VueAPP } from "vue";
+import { createPinia } from "pinia";
+import router from "@/router";
+import App from "@/App.vue";
+import { OpenAPI } from "./api/core/OpenAPI";
+
+const installVuePlugins = (app: VueAPP) => {
+  app.use(createPinia());
+  app.use(router);
+};
+
+export const init = () => {
+  const app = createApp(App);
+  installVuePlugins(app);
+  app.mount("#app");
+
+  // 从 .env 中读取 VITE_API_URL
+  OpenAPI.BASE = import.meta.env.VITE_API_URL;
+
+  // 通常这个无需配置, 因提供的第三方网易云 API 服务端已帮我们处理.
+  // 详细：{网易云 API url}/docs/#/?id=_2-%e9%82%ae%e7%ae%b1%e7%99%bb%e5%bd%95
+  // OpenAPI.TOKEN;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
-import "./assets/main.css";
+import "@/assets/main.css";
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { init } from "@/client/init";
 
-import App from "./App.vue";
-import router from "./router";
-
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+init();
