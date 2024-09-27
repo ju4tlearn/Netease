728600e2219dc19341ab18c92f11407ea1a231d2
ju4tlearn <ju4tlearn@gmail.com>
2024-09-27 17:59:16 +0800

    :art: 初始化整个页面结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,34 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      path: "/login",
+      name: "login",
+      component: () => import("@/views/Login/LoginView.vue"),
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/",
+      name: "index",
+      component: () => import("@/views/IndexView.vue"),
+      children: [
+        {
+          path: "/",
+          name: "home",
+          component: () => import("@/views/Home/HomeView.vue"),
+        },
+        {
+          path: "/profile",
+          name: "profile",
+          component: () => import("@/views/Profile/ProfileView.vue"),
+        },
+      ],
+    },
+    {
+      path: "/playlist",
+      name: "playlist",
+      component: () => import("@/views/Playlist/PlaylistView.vue"),
     },
   ],
 });
